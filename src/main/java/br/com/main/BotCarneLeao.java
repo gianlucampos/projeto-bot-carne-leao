928f1815930636018927c0fc9836fdc994062e13
gianlucampos <gianlucampos97@gmail.com>
2023-04-21 15:48:02 -0300

    Preenchendo dividendos

--- src/main/java/br/com/main/BotCarneLeao.java
+++ src/main/java/br/com/main/BotCarneLeao.java
@@ -4,6 +4,7 @@ import dev.botcity.framework.bot.DesktopBot;
 import dev.botcity.maestro_sdk.BotExecutor;
 import dev.botcity.maestro_sdk.runner.BotExecution;
 import dev.botcity.maestro_sdk.runner.RunnableAgent;
+
 import java.io.IOException;
 
 public class BotCarneLeao extends DesktopBot implements RunnableAgent {
@@ -25,44 +26,71 @@ public class BotCarneLeao extends DesktopBot implements RunnableAgent {
     public void action(BotExecution botExecution) {
 
         try {
-
+            goToRendimentosPage();
+            adicionarRendimentos();
+            adicionarRendimentos();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void rendimentosPage() {
-        if (!find("rendimentosPage", 0.97, 3000)) {
+    private void goToRendimentosPage() {
+        //Acessa pagina rendimentos
+        if (!findText("rendimentosPage", 3000)) {
             notFound("rendimentosPage");
             return;
         }
         click();
-        if (!find( "adicionarRendimento", 0.95, 5000)) {
-            notFound("adicionarRendimento");
+        //Clica pra adicionar rendimento
+        if (!find("adicionarRendimentoText", 0.95, 3000)) {
+            notFound("adicionarRendimentoText");
             return;
         }
         click();
-        if (!findText( "natureza", 230, 3000)) {
+
+    }
+
+    private void adicionarRendimentos() {
+        //Preenche natureza com Outros
+        if (!findText("natureza", 230, 3000)) {
             notFound("natureza");
             return;
         }
         clickRelative(136, 52);
-        keyUp();
-        keyEnter(300);
+        type("Outros");
+        enter();
+
+        //Preenche Data recebimento
         tab();
         paste("01/02/2022");
-        if (!findText( "historico", 230, 3000)) {
+
+        //Preenche Historico
+        if (!findText("historico", 230, 3000)) {
             notFound("historico");
             return;
         }
         clickRelative(125, 51);
         paste("ABOBORA", 500);
+
+        //Preenche Recebido de Exterior
         shiftTab();
         space();
+
+        //Preenche valor
         tab();
         tab();
         paste("10,00");
+
+        //Incluir Rendimento
+        tab();
+        enter();
+
+        //Reinvestir
+        wait(3000);
+        tab();
+        enter();
+
     }
 
     private void notFound(String label) {
@@ -72,6 +100,7 @@ public class BotCarneLeao extends DesktopBot implements RunnableAgent {
     private void loadResources() throws IOException {
         addImage("rendimentosPage", "src/resources/rendimentos/rendimentosPage.png");
         addImage("adicionarRendimento", "src/resources/rendimentos/adicionarRendimento.png");
+        addImage("adicionarRendimentoText", "src/resources/rendimentos/adicionarRendimentoText.png");
         addImage("natureza", "src/resources/rendimentos/natureza.png");
         addImage("historico", "src/resources/rendimentos/historico.png");
     }
